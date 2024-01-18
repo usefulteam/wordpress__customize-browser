ce7afad681dc2a6cbd88b0c09fb96b5569e62c27
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-01-18 19:39:33 +0700

    Update Class.d.ts

--- Class.d.ts
+++ Class.d.ts
@@ -1,6 +1,6 @@
 export abstract class Class {
     static applicator: object;
-    static extend(protoProps: object, classProps: object): Class;
+    static extend(protoProps: object, classProps: object): typeof Class;
     constructor(applicator: object, argsArray: object, options?: object);
     abstract initialize(..._: any[]): void;
     extended(constructor: object): boolean;
