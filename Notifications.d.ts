1168736ae8bf7b7ae536f21a688717191da3d30d
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-01-27 01:22:28 +0700

    add container to notifications

--- Notifications.d.ts
+++ Notifications.d.ts
@@ -2,22 +2,26 @@ import { Notification } from "./Notification";
 import { Values } from "./Values";
 
 export interface Notifications_Options {
-    container?: JQuery | undefined;
-    alt?: boolean | undefined;
+	container?: JQuery | undefined;
+	alt?: boolean | undefined;
 }
 
 export interface Notifications_Get_Options {
-    sort?: boolean | undefined;
+	sort?: boolean | undefined;
 }
 
 export class Notifications extends Values<Notification> {
-    alt: boolean;
-    defaultConstructor: Notification;
-    initialize(options: Notifications_Options): void;
-    count(): number;
-    add(notification: string | Notification, notificationObject?: Notification): Notification;
-    remove(code: string): Notification;
-    get(args: Notifications_Get_Options): Notification[];
-    render(): void;
-    constrainFocus(event: JQuery.Event): void;
+	alt: boolean;
+	defaultConstructor: Notification;
+	initialize(options: Notifications_Options): void;
+	count(): number;
+	add(
+		notification: string | Notification,
+		notificationObject?: Notification
+	): Notification;
+	remove(code: string): Notification;
+	get(args: Notifications_Get_Options): Notification[];
+	render(): void;
+	constrainFocus(event: JQuery.Event): void;
+	container: JQuery;
 }
