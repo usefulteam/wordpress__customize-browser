e20e151a3d43342283d02ec1ebe7b1f097bc6146
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-02-01 01:01:31 +0700

    adjust bind & unbind

--- Events.d.ts
+++ Events.d.ts
@@ -2,6 +2,6 @@ export class Events<T extends keyof any> {
     // TODO: class with statics instead?
     topics: Record<T, JQuery.Callbacks>;
     trigger(id: T): this;
-    bind(id: T): this;
-    unbind(id: T): this;
+    bind(id: T, callback: Function): this;
+    unbind(id: T, callback?: Function): this;
 }
