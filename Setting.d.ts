3c113c2b9282782c47d6eadbfb1d58c5eb55d032
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-02-01 03:45:27 +0700

    correction

--- Setting.d.ts
+++ Setting.d.ts
@@ -11,7 +11,7 @@ export interface Setting_Options {
 export class Setting<T> extends Value<T> {
 	defaults: Setting_Options;
 	id: string;
-	bind(...args: Array<JQuery.TypeOrArray<(to: T, from: T) => void>>): this;
+	bind: (...args: Array<JQuery.TypeOrArray<(to: T, from: T) => void>>) => this;
 	initialize(
 		id?: string,
 		value?: any,
